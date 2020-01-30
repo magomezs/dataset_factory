09ae7847379e76667c8fecebb7944bb4cd6c60f1
magomezs <mariajgomezsilva@gmail.com>
2020-01-30 13:16:33 +0100

    Update data_factory_from_mot.h

--- data_factory_from_mot/data_factory_from_mot.h
+++ data_factory_from_mot/data_factory_from_mot.h
@@ -4,9 +4,14 @@
 #include <string>
 
 std::string int2str(int);
-void get_samples(std::string, float);
-void create_pair_data(std::string, int, int, int, int, float);
-void create_triplet_data(std::string, int, int, int, int, float);
+void create_pair_triplet_data(std::string, int, int, int, float, float);
+void create_pair_data(std::string, int, int, int, int, float, float);
+void create_triplet_data(std::string, int, int, int, int, float, float);
 void create_tracklet_data(std::string, int, int, int, int, int, float);
+void create_contiguous_tracklet_data(std::string, int, int, int, int, int, float, float);
+void create_reid_tracklet_data(std::string, int, int, int, int, int, int, float, float);
+void create_occlusion_tracklet_data(std::string, int, int, int, int, int, int, int, float, float);
+void create_intruders_tracklet_data(std::string, int, int, int, int, int, int, float, float);
+void create_real_tracklet_data(std::string, int, int, int, int, int, int, int, int, float, float);
 
 #endif
